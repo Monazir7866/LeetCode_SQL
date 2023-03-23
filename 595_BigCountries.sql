e94623eaa12ca7816a64f0018b3148a647cb9438
Monazir7866 <monazirhussain722@gmail.com>
2023-03-24 02:46:48 +0530

    sql

---
+++ 595_BigCountries.sql
@@ -0,0 +1,17 @@
+Table: World
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | int     |
++-------------+---------+
+name is the primary key column for this table.
+Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+
+
+
+SELECT NAME,POPULATION,AREA FROM WORLD WHERE AREA>=3000000 OR POPULATION>=25000000;
